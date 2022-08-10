03cf2579c753f6b8ea46a9edfdd52dacfa17bca2
PraneetDighe <86455300+PraneetDighe@users.noreply.github.com>
2022-08-10 17:36:59 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+var slidePosition = 0;
+
+SlideShow();
+
+
+
+function SlideShow() {
+
+  var i;
+
+  var slides = document.getElementsByClassName("Containers");
+
+  for (i = 0; i < slides.length; i++) {
+
+    slides[i].style.display = "none";
+
+  }
+
+  slidePosition++;
+
+  if (slidePosition > slides.length) {slidePosition = 1}
+
+  slides[slidePosition-1].style.display = "block";
+
+  setTimeout(SlideShow, 4000); // Change image every 2 seconds
+
+}
