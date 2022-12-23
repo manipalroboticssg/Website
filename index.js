b7b69055b8e7c406e2bebbec46573a2ada4bd2f9
Mohana Misra <iammohanamisra@gmail.com>
2022-12-23 12:23:58 +0530

    Integrated Swiper

--- index.js
+++ index.js
@@ -1,47 +1,20 @@
-// // let slideIndex = 1;
-// showSlides(slideIndex);
 
-// // Next/previous controls
-// function plusSlides(n) {
-//   showSlides(slideIndex += n);
-// }
+const swiper = new Swiper('.swiper', {
+  autoplay :{
+    delay: 3000, //3 seconds time interval for slides
+    disableOnInteraction: false,
+  },
+  loop: true,
 
-// // Thumbnail image controls
-// function currentSlide(n) {
-//   showSlides(slideIndex = n);
-// }
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
-// function showSlides(n) {
-//   let i;
-//   let slides = document.getElementsByClassName("mySlides");
-//   let dots = document.getElementsByClassName("dot");
-//   if (n > slides.length) {slideIndex = 1}
-//   if (n < 1) {slideIndex = slides.length}
-//   for (i = 0; i < slides.length; i++) {
-//     slides[i].style.display = "none";
-//   }
-//   for (i = 0; i < dots.length; i++) {
-//     dots[i].className = dots[i].className.replace(" active", "");
-//   }
-//   slides[slideIndex-1].style.display = "block";
-//   dots[slideIndex-1].className += " active";
-// }
-
-let slideIndex = 0;
-showSlides();
-
-// Next/previous controls
-let nextButton = document.getElementById("next");
-let prevButton = document.getElementById("prev");
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 3500); // Change image every 3.5 seconds
-}
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
